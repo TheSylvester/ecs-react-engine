61fe51690fcb7e1a1bfdc62007fbee23b141c06d
Sylvester Wong <sylvester@thesylvester.ca>
2022-11-23 21:21:08 -0500

    feat: Refactored Styled Components

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,32 +1,10 @@
 import { useRef, useState } from "react";
-import styled from "styled-components";
 
 import useMousePosition from "../hooks/MousePosition";
-
-const StyledPlayerDiv = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 50px;
-  height: 50px;
-  text-align: center;
-  border-radius: 50%;
-  background-color: red;
-  border: 5px solid black;
-
-  display: none;
-`;
-
-const StyledStartButton = styled.button`
-  text-align: center;
-  position: absolute;
-  margin-left: auto;
-  margin-right: auto;
-  left: 0;
-  right: 0;
-  top: 50%;
-  width: min-content;
-`;
+import {
+  StyledPlayerDiv,
+  StyledStartButton
+} from "../css/StyledGameComponents";
 
 export function Game() {
 
---
+++ src/css/StyledGameComponents.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledPlayerDiv = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  border-radius: 50%;
+  background-color: red;
+  border: 5px solid black;
+
+  display: none;
+`;
+
+export const StyledStartButton = styled.button`
+  text-align: center;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  top: 50%;
+  width: min-content;
+`;
